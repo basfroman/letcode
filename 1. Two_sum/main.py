d94eae65be5c016f63f70159c49267c1ac47e14d
basfroman <basfroman@gmail.com>
2021-02-20 04:13:36 -0800

    twoSum

--- 1. Two_sum/main.py
+++ 1. Two_sum/main.py
@@ -18,3 +18,5 @@ if __name__ == '__main__':
     assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
     assert Solution().twoSum([3, 2, 4], 6) == [1, 2]
     assert Solution().twoSum([3, 3], 6) == [0, 1]
+    assert Solution().twoSum([1, 2, 3, 4, 5], 8) == [2, 4]
+
