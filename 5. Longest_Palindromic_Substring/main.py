6bfc1ef6f76faffbbe41bb9e4f0a4f003f6c143f
basfroman <basfroman@gmail.com>
2021-02-21 22:26:59 -0800

    Median of two sorted arrays

---
+++ 5. Longest_Palindromic_Substring/main.py
@@ -0,0 +1,22 @@
+# Given a string s, return the longest palindromic substring in s.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        result = ''
+        temp = ''
+
+        if not s:
+            return ''
+
+        for i in range(len(s)):
+
+            temp += s[i]
+            print(temp)
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    assert sol.longestPalindrome('babad') == 'bab'
+    assert sol.longestPalindrome('cbbd') == 'bb'
+    assert sol.longestPalindrome('a') == 'a'
+    assert sol.longestPalindrome('ac') == 'a'
